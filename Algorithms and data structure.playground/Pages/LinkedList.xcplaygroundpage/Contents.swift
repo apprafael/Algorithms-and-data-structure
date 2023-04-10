dc0fce9bbbacf264eecbf8a07514cc347ffeca62
Rafael Almeida <apprafael.almeida@gmail.com>
2023-04-10 16:00:01 -0300

    reverse range in linked list

--- Algorithms and data structure.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
+++ Algorithms and data structure.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-public class Node<T> {
+public class Node<T: Equatable> {
     var value: T
     var next: Node<T>?
     var previous: Node<T>?
@@ -8,20 +8,65 @@ public class Node<T> {
     }
 }
 
-public class LinkedList<T> {
+public class LinkedList<T: Equatable> {
     private var head: Node<T>?
     private var tail: Node<T>?
     
     func reverseList() {
         tail = head
+        var oldHead = head
         head = nil
-        
+
         while(tail != nil) {
             let nextHead = tail?.next
             tail?.next = head
             head = tail
             tail = nextHead
         }
+        
+        tail = oldHead
+    }
+    
+    func reverseList(rangeM: T, rangeN: T) {
+        var oldHead = head
+        var oldTail = tail
+        
+        var initialMergeLink: Node<T>? = nil
+        var finalMergeLink: Node<T>? = nil
+        
+        var previousInitialMergeLink: Node<T>? = nil
+        var nextFinalMergeLink: Node<T>? = nil
+        
+        var tempHead = head
+        
+        while(tempHead?.next != nil) {
+            if initialMergeLink == nil && tempHead?.value == rangeM {
+                initialMergeLink = tempHead
+            }
+            
+            if initialMergeLink == nil {
+                previousInitialMergeLink = tempHead
+            }
+            
+            if finalMergeLink == nil && tempHead?.value == rangeN {
+                finalMergeLink = tempHead
+                nextFinalMergeLink = tempHead?.next
+            }
+            
+            tempHead = tempHead?.next
+        }
+        finalMergeLink?.next = nil
+        
+        head = initialMergeLink
+        tail = finalMergeLink
+        
+        reverseList()
+        
+        previousInitialMergeLink?.next = head
+        head = oldHead
+
+        tail?.next = nextFinalMergeLink
+        tail = oldTail
     }
 
     func append(value: T) {
@@ -60,5 +105,5 @@ carBrands.append(value: "Fiat")
 carBrands.append(value: "Chevrolet")
 carBrands.append(value: "Mercedes")
 print(carBrands)
-carBrands.reverseList()
+carBrands.reverseList(rangeM: "Ferrari", rangeN: "Chevrolet")
 print(carBrands)
